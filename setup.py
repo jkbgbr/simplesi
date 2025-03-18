5e99782fe23eb882f5925c14dbd7faea5ea63b13
JakabG <jkbgbr@gmail.com>
2025-03-18 14:26:19 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='simplesi',
+    version='0.1',
+    packages=['tests', 'simplesi'],
+    url='',
+    license='',
+    author='JakabG',
+    author_email='jkbgbr@gmail.com',
+    description='',
+    package_data={
+        'simplesi': ['environments/*'],
+    },
+)
